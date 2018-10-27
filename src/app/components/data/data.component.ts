e1b3186186ddd95ac374d2d64757d6f879be68dc
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2018-10-27 01:17:50 -0300

    data, continuando validaciones(17)

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -14,9 +14,10 @@ export class DataComponent  {
   forma: FormGroup;
   constructor() {
       this.forma = new FormGroup({
-        'nombre': new FormControl('fernando'),
-        'apellido': new FormControl(),
-        'correo': new FormControl()
+        'nombre': new FormControl('',[Validators.required,Validators.minLength(3)]),
+        'apellido': new FormControl('',Validators.required),
+        'correo': new FormControl('', [
+          Validators.required, Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")])
       });
 
    }
