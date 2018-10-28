b9245de8d3eea6e5ccaefffe5439be92cfb73052
franciscoLagos18615 <francisco.lagos.s@usach.cl>
2018-10-27 21:26:00 -0300

    data validadores asincronos

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 import {FormGroup, FormControl, Validators, FormArray} from '@angular/forms';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-data',
@@ -33,6 +36,7 @@ export class DataComponent  {
           'pasatiempos': new FormArray([
             new FormControl('correr', Validators.required)
           ]),
+          'username': new FormControl('', Validators.required,this.existeUsuario),
           'password1': new FormControl('', Validators.required),
           'password2': new FormControl()
 
@@ -67,6 +71,23 @@ noIgual(control: FormControl): any{
     return {noiguales: true };
   }
   return null;
+}
+
+existeUsuario(control: FormControl): Promise<any>|Observable<any>{
+  let promesa = new Promise(
+    (resolve, reject)=>{
+      setTimeout(() => {
+        if(control.value==="strider"){
+          resolve({existe:true})
+        }else{
+          resolve(null)
+        }
+
+      }, 3000);
+
+    }
+  );
+return promesa;
 }
 
 guardarCambios() {
